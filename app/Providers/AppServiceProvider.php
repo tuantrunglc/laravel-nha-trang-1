20ba0792d594e0abbe5a87f60af3a1c667093cb9
tuantrung128 <tuantrunglc@gmail.com>
2024-04-08 17:44:53 +0700

    Full

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ namespace App\Providers;
 use App\Models\User;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Notification;
 
 class AppServiceProvider extends ServiceProvider
 {
